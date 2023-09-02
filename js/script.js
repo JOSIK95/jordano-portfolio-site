eb5f04ab038f9768d4304e2804057b5c80f82f42
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-09-02 09:45:01 -0400

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('.button');
 button.addEventListener('mouseover', () => {
     button.classList.add = 'button-hover';
 
