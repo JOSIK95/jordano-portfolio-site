66b05b5c5b94ead29e0c488285d29b9045df72b9
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-09-06 16:29:02 -0400

    alert added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert("hello!");
+// get current date/time
+const time = new Date().getHours();
+
+const greeting = "Hello!";
+if (time < 12) {
+  greeting = "Good morning";
+} else if (time < 18) {
+  greeting = "Good afternoon";
+} else {time < 22) {
+  greeting = "Good evening";
+} else {
+    greeting = "Why are you still up? Either way,";
+};
+
+alert('${greeting}, "Welcome to my website!"');
