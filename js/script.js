7e50edd40819afcd9d1c83cdf05a74ecb40cccf4
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-09-06 16:32:39 -0400

    js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ if (time < 12) {
   greeting = "Good morning";
 } else if (time < 18) {
   greeting = "Good afternoon";
-} else {time < 22) {
+} else if (time < 22) {
   greeting = "Good evening";
 } else {
-    greeting = "Why are you still up? Either way,";
+    greeting = "Why are you still up? Either way";
 };
 
 alert('${greeting}, "Welcome to my website!"');
