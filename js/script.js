70f61365d3f2075871c10a1dbc0305a1123079a1
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-09-09 08:40:07 -0400

    more js

--- js/script.js
+++ js/script.js
@@ -18,26 +18,33 @@ greetingTime.innerHTML = `<div>${greeting}</div>`;
 
 // hover effects
 
+// h1
 var headlineHover = document.querySelector(".hello");
 headlineHover.addEventListener("mouseover", function() {
     headlineHover.classList.add("hello-hover");
 });
 var headlineHover = document.querySelector(".hello");  
 headlineHover.addEventListener("mouseout", function() {
-    headlineHover.classList.add("hello-hover-out");
+    headlineHover.classList.remove("hello-hover");
 });
 
 
+// h2 project title
+var projectTitleHover = document.querySelector("#work h2");
+projectTitleHover.addEventListener("mouseover", function() {
+    projectTitleHover.classList.add("#work-h2-hover");
+});
+var projectTitleHover = document.querySelector("#work h2");
+projectTitleHover.addEventListener("mouseout", function() {
+    projectTitleHover.classList.remove("#work-h2-hover");
+});
 
-
-
-
-
+// portfolio header
 var portfolioHeader = document.querySelector(".portfolio-header");
 portfolioHeader.addEventListener("mouseover", function() {
-    portfolioHeader.classList.add(".hover-portfolio-header");
+    portfolioHeader.classList.add("h3.hover-portfolio-header");
 });
  var portfolioHeader = document.querySelector(".portfolio-header");
 portfolioHeader.addEventListener("mouseout", function() {
-    portfolioHeader.classList.add(".hover-portfolio-header-out");
+    portfolioHeader.classList.remove("h3.hover-portfolio-header");
 });
