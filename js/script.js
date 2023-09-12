77f06d76bac0d9e3034e6d2f8d118bfa20c6d216
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-09-12 15:48:41 -0400

    finalizing

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-// get current date/time----------------------------------------------
+// get current date/time
 var greetingTime = document.querySelector(".greeting-time");
 
 var time = new Date().getHours();
 
 var greeting = "Hello!"
 if (time < 12) {
-  greeting = "Good morning";
+    greeting = "Good morning";
 } else if (time <= 13) {
-  greeting = "Good afternoon";
+    greeting = "Good afternoon";
 } else if (time <= 22) {
-  greeting = "Good evening";
+    greeting = "Good evening";
 } else {
     greeting = "Good night";
 };
 
 greetingTime.innerHTML = `<div>${greeting}</div>`;
 
-// hover effects----------------------------------------------
 
 // h1
 
 var headlineHover = document.querySelector(".hello");
-headlineHover.addEventListener("mouseover", function() {
+headlineHover.addEventListener("mouseover", function () {
     headlineHover.classList.add("hello-hover");
 });
-var headlineHover = document.querySelector(".hello");  
-headlineHover.addEventListener("mouseout", function() {
+var headlineHover = document.querySelector(".hello");
+headlineHover.addEventListener("mouseout", function () {
     headlineHover.classList.remove("hello-hover");
 });
 
 // time of day greeting hover
 
 var timeOfDayHover = document.querySelector(".greeting-time");
-timeOfDayHover.addEventListener("mouseover", function() {
+timeOfDayHover.addEventListener("mouseover", function () {
     timeOfDayHover.classList.add("greeting-time-hover");
 });
 var timeOfDayHover = document.querySelector(".greeting-time");
-timeOfDayHover.addEventListener("mouseout", function() {
+timeOfDayHover.addEventListener("mouseout", function () {
     timeOfDayHover.classList.remove("greeting-time-hover");
 });
 
@@ -51,10 +50,10 @@ smileyFace.style.color = "slategray";
 
 aboutHeading.appendChild(smileyFace);
 
-aboutHeading.addEventListener("mouseover", function() {
+aboutHeading.addEventListener("mouseover", function () {
     smileyFace.style.opacity = "1";
 });
-aboutHeading.addEventListener("mouseout", function() {
+aboutHeading.addEventListener("mouseout", function () {
     smileyFace.style.opacity = "0";
 });
 
